339abf87de3f9a31a14c77b6d8a888d01b8b3823
UmaSankarRebba <umasankarrebba@gmail.com>
2026-03-15 21:15:54 +0530

    Changes Required

--- roboshop.sh
+++ roboshop.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-
 SG_ID="sg-0c2d3e7831a625405"
 AMI_ID="ami-0220d79f3f480ecf5"
 
 for instance in $@
 do
-instance_id=$(aws ec2 run-instances \
+    instance_id=$(aws ec2 run-instances \
 --image-id $AMI_ID \
 --instance-type t3.micro \
 --security-group-ids $SG_ID \
 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
---query 'Instance[0].InstanceId' \
+--query 'Instances[0].InstanceId' \
 --output text )   
 
 if [ $instance == "frontend" ]; then
