a1c431e974a33c56156078cd20649a8adddca832
UmaSankarRebba <umasankarrebba@gmail.com>
2026-03-15 18:51:32 +0530

    Changes Required

---
+++ roboshop.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+SG_ID="sg-0c2d3e7831a625405"
+AMI_ID="ami-0220d79f3f480ecf5"
+
+for instance in $@
+do
+instance_id=$(aws ec2 run-instances \
+--image-id $AMI_ID \
+--instance-type t3.micro \
+--security-group-ids $SG_ID \
+--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"\
+--query "Reservations[].Instances[].PublicIpAddress" \
+--output text )   
+
+if [ $instance == "frontend" ]; then
+ IP=$( aws ec2 describe-instances \    
+ --instance-ids $instance_id \
+ --query "Reservations[].Instances[].PublicIpAddress" \
+ --output text
+  )
+else
+ IP=$(
+    aws ec2 describe-instances \    
+ --instance-ids $instance_id \
+ --query "Reservations[].Instances[].PublicIpAddress" \
+ --output text
+ )
+ fi
+done
