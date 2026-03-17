af1856b01bdf21f944429bf7f06ea578e5d1b53a
UmaSankarRebba <umasankarrebba@gmail.com>
2026-03-17 11:52:56 +0530

    Changes Required

--- roboshop.sh
+++ roboshop.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 SG_ID="sg-0c2d3e7831a625405"
 AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z06532621BV9L2WWM6NW4"
+DOMAIN_NAME="rebba.online"
 R="\e[31m"
 N="\e[0m"
 
@@ -20,6 +22,7 @@ if [ $instance == "frontend" ]; then
  --query "Reservations[].Instances[].PublicIpAddress" \
  --output text
   )
+  RECORD_NAME="$DOMAIN_NAME" #rebba.online
 else
  IP=$(
     aws ec2 describe-instances \
@@ -27,6 +30,31 @@ else
  --query "Reservations[].Instances[].PrivateIpAddress" \
  --output text
  )
+ RECORD_NAME="$instance.$DOMAIN_NAME" #backend.rebba.online
 fi
 echo -e " $R Your IP Address is $IP $N"
+
+aws route53 change-resource-record-sets \
+ --hosted-zone-id $ZONE_ID \
+ --change-batch '
+ {
+  "Comment": "Update record to reflect new IP address",
+  "Changes": [
+    {
+      "Action": "UPSERT",
+      "ResourceRecordSet": {
+        "Name": "'$RECORD_NAME'",
+        "Type": "A",
+        "TTL": 1,
+        "ResourceRecords": [
+          {
+            "Value": "'$IP'"
+          }
+        ]
+      }
+    }
+  ]
+}
+'
+echo "record updated for $instance"
 done
