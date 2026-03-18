30d665e6eaf168de299343e049e108f9e5ef6c0c
UmaSankarRebba <umasankarrebba@gmail.com>
2026-03-18 11:40:21 +0530

    Changes required

---
+++ catalogue.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+USER_ID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILES="/var/log/shell-roboshop/$0.log"
+R="\e[31m"
+G="\e[32m"
+C="\e[36m"
+N="\e[0m"
+
+if [ $USER_ID -ne 0 ]; then
+ echo -e " $R You dont have permission to access this operation $N.$G Please contact sudo Admin $N"
+ exit 1
+fi
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+     echo -e "$R $2 is Failure $N" | tee -a $LOGS_FILES
+     exit 1
+    else
+     echo -e "$G $2 is Success $N" | tee -a $LOGS_FILES
+    fi
+}
+
+dnf module disable nodejs -y
+VALIDATE $? "Disabling nodejs default version"
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "ENABLE NODEJS 20 version"
+
+dnf install nodejs -y
+VALIDATE $? "Installing nodejs"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+VALIDATE $? "Creating system user"
+
+mkdir /app 
+VALIDATE $? "creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+VALIDATE $? "Downloading catalogue code"
