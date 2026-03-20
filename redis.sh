b8ba7c9128ff1a93a6bd5b960513afbf928059bd
UmaSankarRebba <umasankarrebba@gmail.com>
2026-03-20 09:21:42 +0530

    Changes Required

---
+++ redis.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+USER_ID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILES="/var/log/shell-roboshop/$0.log"
+R="\e[31m"
+G="\e[32m"
+C="\e[36m"
+N="\e[0m"
+
+if [ $USER_ID -ne 0 ]; then
+ echo -e " $R You dont have permission to access this operation $N.$G Please contact sudo Admin $N"
+ exit 1
+fi
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+     echo -e "$R $2 is Failure $N" | tee -a $LOGS_FILES
+     exit 1
+    else
+     echo -e "$G $2 is Success $N" | tee -a $LOGS_FILES
+    fi
+}
+dnf module disable redis -y
+VALIDATE $? "disable deafault version of redis"
+
+dnf module enable redis:7 -y
+VALIDATE $? "enable redis 7 version"
+
+dnf install redis -y 
+VALIDATE $? "install redis "
+
+sed -i 's/127.0.0.1/0.0.0.0/g' -e  '/protected-mode/ c protected--mode on' /etc/redis/redis.conf
+
+systemctl enable redis
+VALIDATE $? "enable redis"
+
+systemctl start redis
+VALIDATE $? "start rediss"
