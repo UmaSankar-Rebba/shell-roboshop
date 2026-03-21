2184b98c817668e56454bcbdab395c943efb3b37
UmaSankarRebba <umasankarrebba@gmail.com>
2026-03-21 10:49:22 +0530

    Changes Required

--- user.sh
+++ user.sh
@@ -6,6 +6,7 @@ R="\e[31m"
 G="\e[32m"
 C="\e[36m"
 N="\e[0m"
+SCRIPT_DIR=$PWD
 
 if [ $USER_ID -ne 0 ]; then
  echo -e " $R You dont have permission to access this operation $N.$G Please contact sudo Admin $N"
@@ -29,19 +30,34 @@ VALIDATE $? "dsable and enable nodejs"
 dnf install nodejs -y
 VALIDATE $? "install nodejs"
 
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
-VALIDATE $? "creating system user"
+id roboshop
+if [ $? -ne 0 ]; then {
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    VALIDATE $? "creating system user"
+}
+else
+ echo -e "$C User already exists....! Skipping $N"
+
 
 mkdir -p /app
 VALIDATE $? "Creating new folder"
 
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip 
-cd /app 
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
+VALIDATE $? "Downloading the code"
+
+cd /app
+VALIDATE $? "changing to app dir"
+
+rm -rf /app/*
+VALIDATE $? "removing the existing files"
+
 unzip /tmp/user.zip
 VALIDATE $? "unzipping file"
 
 npm install
 VALIDATE $? "installing"
+
+cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service
 
 systemctl daemon-reload
 VALIDATE $? "reloading"
