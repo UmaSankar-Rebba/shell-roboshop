277584a94fe80fd7f6f260a09796fc8c53cb154b
UmaSankarRebba <umasankarrebba@gmail.com>
2026-03-21 12:32:51 +0530

    Changes Required

--- shipping.sh
+++ shipping.sh
@@ -28,7 +28,7 @@ dnf install maven -y
 VALIDATE $? "Installing maven"
 
 id roboshop
-if [ $? -ne 0 ]; then{
+if [ $? -ne 0 ]; then {
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
     VALIDATE $? "User Added"
 }
