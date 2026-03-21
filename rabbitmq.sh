25431b7ea7ef8eef39d967e624baddf641b8c114
UmaSankarRebba <umasankarrebba@gmail.com>
2026-03-21 12:59:23 +0530

    Changes Required

---
+++ rabbitmq.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+USER_ID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILES="/var/log/shell-roboshop/$0.log"
+R="\e[31m"
+G="\e[32m"
+C="\e[36m"
+N="\e[0m"   
+
+if [ $USER_ID -ne 0 ]; then
+ echo -e " $R You dont have permission to access this operation $N.$G Please contact sudo Admin $N"
+ exit 1
+fi
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+     echo -e "$R $2 is Failure $N" | tee -a $LOGS_FILES
+     exit 1
+    else
+     echo -e "$G $2 is Success $N" | tee -a $LOGS_FILES
+    fi
+}
+
+cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+VALIDATE $? "Repo copied"
+
+dnf install rabbitmq-server -y
+VALIDATE $? "Installing rabbitmq"
+
+systemctl enable rabbitmq-server
+VALIDATE $? "enable rabbitmq"
+
+systemctl start rabbitmq-server
+VALIDATE $? "Start rabbitmq"
+
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+VALIDATE $? "created user and gain permissions"
