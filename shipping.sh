91ed734994a294092910a2ec8a98fdce02f06ad7
UmaSankarRebba <umasankarrebba@gmail.com>
2026-03-22 11:41:36 +0530

    Changes Required

--- shipping.sh
+++ shipping.sh
@@ -67,14 +67,14 @@ VALIDATE $? "installing mysql"
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
 if [ $? -ne 0 ]; then
 
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOGS_FILE
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILES
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOGS_FILES
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILES
     VALIDATE $? "Loaded data into MySQL"
 else
     echo -e "data is already loaded ... $Y SKIPPING $N"
 fi
 
-systemctl enable shipping &>>$LOGS_FILE
+systemctl enable shipping &>>$LOGS_FILES
 systemctl start shipping
 VALIDATE $? "Enabled and started shipping"
